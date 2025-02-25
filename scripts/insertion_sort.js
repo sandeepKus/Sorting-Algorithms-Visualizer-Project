e6458ba408d712349535501f33ef2a93a5c9e5ea
sandeepKus <155589569+sandeepKus@users.noreply.github.com>
2025-02-25 22:35:46 +0530

    Update insertion_sort.js

--- scripts/insertion_sort.js
+++ scripts/insertion_sort.js
@@ -1,9 +1,4 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
+
 
 function Insertion()
 {
@@ -56,9 +51,4 @@ function Insertion()
     enable_buttons();
 }
 
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
+
